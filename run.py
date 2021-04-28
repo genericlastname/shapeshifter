d5770e844bd9926b459bddbe4f29d8e24ede80b8
Kyle Brown <brownkylej@gmail.com>
2021-04-28 15:57:44 -0400

    Start template

---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", message="by Kyle Brown");
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
