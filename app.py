d95c8cd5f64197b8227e53302681011dbafe94f3
Kyle Brown <brownkylej@gmail.com>
2021-04-28 23:54:33 -0400

    Extract data from parse tree

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from lark import Lark
 app = Flask(__name__)
 
 grammar = r'''
-    start : [colorn] SHAPE ["at " pair] ["size of " pair]
+    start : [colorn] shapen ["at " pair] ["size of " pair]
     pair  : NUMBER","NUMBER
     colorn: COLOR
           | hex
     hex   : "#"(DIGIT|LETTER)(DIGIT|LETTER)(DIGIT|LETTER)(DIGIT|LETTER)(DIGIT|LETTER)(DIGIT|LETTER)
+    shapen: SHAPE
 
     COLOR : LETTER+
     SHAPE : LETTER+
@@ -25,8 +26,39 @@ parser = Lark(grammar)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        color='#000000'
+        x = 0
+        y = 0
+        w = 100
+        h = 100
+        shape = None
+
         statement = request.form.get('statement')
-        print(parser.parse(statement).pretty())
+        parsed = parser.parse(statement)
+
+        # print(parsed.children[1].children[1])
+        if parsed.children[0].data == 'colorn':
+            color_name = parsed.children[0].children[0]
+            if color_name == 'red':
+                color = '#ff0000'
+            elif color_name == 'blue':
+                color = '#0000ff'
+            elif color_name == 'green':
+                color = '#00ff00'
+            elif color_name == 'yellow':
+                color = '#ffff00'
+            else:
+                color = color_name
+        elif parsed.children[0].data == 'shapen':
+            shape = parsed.children[0].children[0]
+
+        if len(parsed.children) > 1:
+            x = int(parsed.children[1].children[0])
+            y = int(parsed.children[1].children[1])
+        elif len(parsed.children) > 2:
+            w = int(parsed.children[2].children[0])
+            h = int(parsed.children[2].children[1])
+
     return render_template('index.html');
 
 
